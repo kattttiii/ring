33e482bf1a5a9011833dc9f74520588eaa1d4b1c
kattttiii <144277414+kattttiii@users.noreply.github.com>
2024-02-15 10:34:13 +0300

    Add files via upload

---
+++ 3 pract.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+double RingArea(double radius) {
+    return 3.14 * radius * radius;
+}
+
+double RingArea(double innerRadius, double outerRadius) {
+    return RingArea(outerRadius) - RingArea(innerRadius);
+}
+
+int main() {
+    setlocale(0, "");
+    double inRadius, outRadius;
+
+    std::cout << "Введите значение внутренного радиуса: ";
+    std::cin >> inRadius;
+
+    std::cout << "Введите значение внешнего радиуса: ";
+    std::cin >> outRadius;
+
+    if (outRadius < inRadius) {
+        std::cout << "Ошибка: внешний радиус не может быть меньше" << std::endl;
+
+        std::cout << "Введите значение внутренного радиуса: ";
+        std::cin >> inRadius;
+
+        std::cout << "Введите значение внешнего радиуса: ";
+        std::cin >> outRadius;
+    }
+
+    double ring_Area = RingArea(inRadius, outRadius);
+
+    std::cout << "Площадь кольца с внутренним радиусом " << inRadius
+        << " и внешним радиусом " << outRadius << " равна " << ring_Area << std::endl;
+
+    return 0;
+}
